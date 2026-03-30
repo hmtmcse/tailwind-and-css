ec1ef59b7a70ba54d423ef723116866dd3b8bfa3
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2026-03-30 22:02:26 +0600

    Working on add Tailwind css

--- ui-project-trial/src/application.tsx
+++ ui-project-trial/src/application.tsx
@@ -1,15 +1,36 @@
 import "./assets/css/style.css"
 import {Button} from "./shadcn/ui/button.tsx";
 import {ButtonGroup} from "./shadcn/ui/button-group.tsx";
+import {useEffect, useState} from "react";
 
-export default function Application(){
+export default function Application() {
+    const [dark, setDark] = useState(false);
+
+    useEffect(() => {
+        const root = document.documentElement;
+
+        if (dark) {
+            root.classList.add("dark");
+        } else {
+            root.classList.remove("dark");
+        }
+    }, [dark]);
+
     return (
         <>
             <div className={"text-red-500 font-bold"}>
                 Bismillah
             </div>
 
-            <Button variant={"secondary"} size={"sm"}>Button</Button>
+            <Button className={"mt-5"} variant={"secondary"} size={"sm"} onClick={() => setDark(!dark)}>Dark</Button>
+
+            <br/>
+            <br/>
+
+            <Button className={"mt-5"} variant={"secondary"} size={"sm"}>Button</Button>
+
+            <br/>
+            <br/>
 
             <ButtonGroup>
                 <Button variant="outline" size={"sm"}>Archive</Button>
