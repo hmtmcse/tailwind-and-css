573d13f3d995f3ab6a51c2af654d8d7b19e327e2
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2026-03-30 21:53:57 +0600

    Working on add Tailwind css

--- ui-project-trial/src/application.tsx
+++ ui-project-trial/src/application.tsx
@@ -1,5 +1,6 @@
 import "./assets/css/style.css"
 import {Button} from "./shadcn/ui/button.tsx";
+import {ButtonGroup} from "./shadcn/ui/button-group.tsx";
 
 export default function Application(){
     return (
@@ -8,7 +9,12 @@ export default function Application(){
                 Bismillah
             </div>
 
-            <Button variant={"secondary"}>Button</Button>
+            <Button variant={"secondary"} size={"sm"}>Button</Button>
+
+            <ButtonGroup>
+                <Button variant="outline" size={"sm"}>Archive</Button>
+                <Button variant="outline" size={"sm"}>Report</Button>
+            </ButtonGroup>
 
         </>
     )
